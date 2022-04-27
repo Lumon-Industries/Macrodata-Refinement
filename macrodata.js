7ecc160a295d7df92a2575fe32c4d5fb79975900
Joseph Horton <josephhorton95@gmail.com>
2022-04-27 12:17:09 +0100

    Add filenames, methods for setting and reading to local storage

---
+++ macrodata.js
@@ -0,0 +1,60 @@
+// TODO: add more file names
+const files = [
+  'Siena',
+  'Nanning',
+  'Narva',
+  'Ocula',
+  'Kingsport',
+  'Labrador',
+  'Le Mars',
+  'Longbranch',
+  'Moonbeam',
+  'Minsk',
+  'Dranesville',
+];
+
+const macrodataKey = 'macrodata';
+
+const emptyBins = [
+  {WO: 0, FC: 0, DR: 0, MA: 0},
+  {WO: 2, FC: 0, DR: 0, MA: 0},
+  {WO: 0, FC: 0, DR: 0, MA: 0},
+  {WO: 0, FC: 0, DR: 0, MA: 0},
+  {WO: 0, FC: 0, DR: 0, MA: 0}
+ ];
+
+// would it be better to wrap this functionality in a class?
+
+const fetchFile = (filename) => {
+  if (storageAvailable("localStorage")) {
+    const res = localStorage.getItem(macrodataKey);
+    if (res) {
+      console.log('found', res);
+      return JSON.parse(res);
+    } else {
+      return assignFile();
+    }
+  }
+}
+
+const assignFile = () => {
+  if (storageAvailable("localStorage")) {
+    const filename = files[Math.floor(Math.random() * files.length)];
+    console.log('assigning', filename);
+    const macrodata = {
+      file: filename,
+      bins: emptyBins
+    }
+    localStorage.setItem(macrodataKey, JSON.stringify(macrodata));
+    return macrodata;
+  }
+}
+
+const updateFileProgress = (macrodataFile) => {
+  console.log('update', macrodataFile)
+  localStorage.setItem(macrodataKey, JSON.stringify(macrodataFile));
+}
+
+const resetFileStorage = () => {
+  localStorage.removeItem(macrodataKey);
+}
