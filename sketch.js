e55b79108f1c0877aeeaef1ec8ca9d2d144bbe14
Joseph Horton <josephhorton95@gmail.com>
2022-05-02 16:09:29 +0100

    Update share string to use stored time (in minutes)

--- sketch.js
+++ sketch.js
@@ -175,8 +175,8 @@ function setup() {
       }
       thenumbers += '\n';
     }
-    const timeStr = completedTime.toLocaleString('en-US');
-    const msg = `In refining ${macrodataFile.coordinates} (${macrodataFile.fileName}) in ${timeStr} milliseconds I have brought glory to the company.
+    const timeStr = (secondsSpentRefining/60).toFixed(2).toLocaleString('en-US');
+    const msg = `In refining ${macrodataFile.coordinates} (${macrodataFile.fileName}) in ${timeStr} minutes I have brought glory to the company.
 Praise Kier.
 ${thenumbers}#mdrlumon #severance 🧇🐐🔢💯
 lumon-industries.com`;
