e31b4dac99a8deb0b875f3a7b12fbfa18159f6af
Joseph Horton <josephhorton95@gmail.com>
2022-05-03 15:21:32 +0100

    Update time string to be in h:m:s:ms format

--- utils.js
+++ utils.js
@@ -25,5 +25,8 @@ const isTouchScreenDevice = () => {
 }
 // Takes a value in seconds, returns hh:mm:ss:ms
 const createTimeString = (seconds) => {
-  return new Date(seconds * 1000).toISOString().substring(11, 23);
+  const baseString = new Date(seconds * 1000).toISOString().substring(11, 23);
+  const hhmm = baseString.split(':');
+  const ssms = hhmm[2].split('.');
+  return `${hhmm[0]}h ${hhmm[1]}m ${ssms[0]}s ${ssms[1]}ms`;
 }
